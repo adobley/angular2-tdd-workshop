8a4e00dfb2c3bff309abadeff9634c520c1e300e
q2uabo <?yCQ6R3V>
2016-11-15 19:05:03 -0500

    Aidan - code cleanup preparing for testing, probably a bunch of stuff wrong

--- src/client/app/app.component.e2e-spec.ts
+++ src/client/app/app.component.e2e-spec.ts
@@ -5,19 +5,19 @@ describe('App', () => {
   });
 
   it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Welcome to angular2-seed!');
+    expect(browser.getTitle()).toEqual('The First Ever ToDo App');
   });
 
-  it('should have <nav>', () => {
-    expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
+  xit('should have <nav>', () => {
+    expect(element(by.css('td-app td-navbar nav')).isPresent()).toEqual(true);
   });
 
-  it('should have correct nav text for Home', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('HOME');
+  xit('should have correct nav text for Home', () => {
+    expect(element(by.css('td-app td-navbar nav a:first-child')).getText()).toEqual('HOME');
   });
 
-  it('should have correct nav text for About', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:nth-child(2)')).getText()).toEqual('ABOUT');
+  xit('should have correct nav text for About', () => {
+    expect(element(by.css('td-app td-navbar nav a:nth-child(2)')).getText()).toEqual('ABOUT');
   });
 
 });
--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { Config } from './shared/index';
 import './operators';
 
-/**
- * This class represents the main application component. Within the @Routes annotation is the configuration of the
- * applications routes, configuring the paths for the lazy loaded components (HomeComponent, AboutComponent).
- */
 @Component({
   moduleId: module.id,
-  selector: 'sd-app',
-  templateUrl: 'app.component.html',
+  selector: 'td-app',
+  template: `<h1>Angular 2 TDD</h1>`
 })
 
 export class AppComponent {
-  constructor() {
-    console.log('Environment config', Config);
-  }
 }
