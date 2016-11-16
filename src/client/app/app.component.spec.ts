7b2b56f82131021fa0a91646576a22e93d391e79
q2uabo <?yCQ6R3V>
2016-11-16 13:34:04 -0500

    Aidan - Fix up a bunch of the problems I made and issues with dependencies changing

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -3,35 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
-import {
-  async
-} from '@angular/core/testing';
-import {
-  Route
-} from '@angular/router';
-import {
-  RouterTestingModule
-} from '@angular/router/testing';
+import { async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ToolbarComponent } from './shared/toolbar/toolbar.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 
 export function main() {
 
   describe('App component', () => {
 
-    let config: Route[] = [
-      { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent }
-    ];
     beforeEach(() => {
       TestBed.configureTestingModule({
-        imports: [FormsModule, RouterTestingModule.withRoutes(config)],
-        declarations: [TestComponent, ToolbarComponent,
-          NavbarComponent, AppComponent,
-          HomeComponent, AboutComponent],
+        imports: [FormsModule],
+        declarations: [TestComponent, AppComponent],
         providers: [
           { provide: APP_BASE_HREF, useValue: '/' }
         ]
@@ -54,7 +36,7 @@ export function main() {
 
 @Component({
   selector: 'test-cmp',
-  template: '<sd-app></sd-app>'
+  template: '<td-app></td-app>'
 })
 
 class TestComponent {
